8f4b1f6eabe3997822e30a07a33a813c6a991e2d
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-11-06 04:08:11 +0800

    Output Hasil

--- tugas2ps.cpp
+++ tugas2ps.cpp
@@ -8,4 +8,21 @@ int main(int argc, char** argv){
 	cout <<"1.Persegi panjang\n";
 	cout <<"2.Segitiga\n";
 	cin >>pilihan;
+	
+	switch(pilihan){
+		case 1:
+		cout<<"masukan panjang =";cin>>panjang;
+		cout<<"masukan lebar =";cin>>lebar;
+		luas =panjang * lebar;
+		cout<<"luas persegi panjang="<<luas<<"\n";
+		system("pause");
+		break;
+		case 2:
+		cout<<"Masukkan panjang alas Segitiga=";cin>>alas;
+		cout<<"Masukkan tinggi Segitiga=";cin>>tinggi;
+		luas=0.5*alas*tinggi;
+		cout<<"luas Segitiga ="<<luas<<"\n";
+		system("pause");
+		
+	}
 }
