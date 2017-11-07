e81f5ed020c5506b12f08b1d6f7227ee614d1722
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-11-07 17:42:19 +0800

    Input dan Output hasil nilai

--- tugas4pnn.cpp
+++ tugas4pnn.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
+	int x;
 	
+	cout <<"Masukkan Nilai : ";cin >>x;
+	
+	if (x>0){
+		cout <<"Nilai positif";
+	}else if (x<0){
+		cout <<"Nilai negatif";
+	}else {
+		cout <<"Nilai nol !!";
+	}
 }
