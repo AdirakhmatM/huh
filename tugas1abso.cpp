582688c3cf0a4a3b5de9f237d323975638d77414
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-11-07 18:04:19 +0800

    Input Nilai variabel

--- tugas1abso.cpp
+++ tugas1abso.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main(){
+	int w,x,y;
 	
+	cout <<"Perhitungan Selisih Nilai Absolute"<<endl;
+	cout <<"Masukkan Nilai X	: ";cin>>x;
+	cout <<"Masukkan Nilai Y	: ";cin>>y;
 }
