a422db92db2ed05c230d01d953b29c081f2137d7
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-11-07 17:26:09 +0800

    Input dan Output Nilai

--- tugas5bb.cpp
+++ tugas5bb.cpp
@@ -3,5 +3,9 @@
 using namespace std;
 
 int(){
-
+	double tinggi, berat;
+	
+	cout <<"Perhitungan berat badan\n";
+	cout <<"Masukkan berat badan : ";cin >>berat;
+	cout <<"Masukkan tinggi badan : ";cin >>tinggi;
 }
