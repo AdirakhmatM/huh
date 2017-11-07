63e66adcf88c1dd04112e4a7343a750d0395fd45
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-11-07 17:58:22 +0800

    Menginput Nilai dan Output nilai variabel

--- tugas3gaji.cpp
+++ tugas3gaji.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main(){
+	int gpkk,jk;
+	int lbr,gt,pjk,gk;
 	
+	cout <<"Perhitungan Gaji Karyawan "<<endl;
+	cout <<"Masukkan Gaji Pokok : Rp. ";cin >>gpkk;
+	cout <<"Masukkan Jam Kerja : ";cin >>jk;
+	lbr=jk-40;
+	gt=(lbr*1.5+40)*gpkk;
+	pjk=gt*0.15;
+	gk=gt-pjk;
+	
+	cout <<"Hasil Perhitungan Gaji"<<endl;
+	cout <<"Gaji Pokok	: Rp. "<<gpkk<<endl;
+	cout <<"Jam Kerja 	: "<<jk<<endl;
+	cout <<"Jam Lembur	: "<<lbr<<endl;
+	cout <<"Gaji sebelum dipotong pajak : Rp. "<<gt<<endl;
+	cout <<"Total Gaji 	: Rp. "<<gk;
 }
