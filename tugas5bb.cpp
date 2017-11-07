8afc7a6798a390bb3dce800bdcb619163778c295
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-11-07 17:36:13 +0800

    Output hasil perhitungan

--- tugas5bb.cpp
+++ tugas5bb.cpp
@@ -9,4 +9,15 @@ int main(){
 	cout <<"Masukkan berat badan : ";cin >>berat;
 	cout <<"Masukkan tinggi badan : ";cin >>tinggi;
 	
+	cout <<"Hasil \n";
+	cout <<"Berat badan : "<<berat<<endl;
+	cout <<"tinggi badan : "<<tinggi<<endl;
+	
+	if (berat<tinggi/2.5){
+		cout <<"Underweight";
+	}else if (tinggi/2.3<berat){
+		cout <<"Overweight";
+	}else if (tinggi/2.5 <=berat <=tinggi/2.3){
+		cout <<"Normal!!";
+	}
 }
